9edb143df7c2c27addb3692cde75e60d28cb34ca
Edward Brown <edward.brown.london@gmail.com>
2015-04-20 22:27:00 +0100

    board can place pieces

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,18 @@ class Board
   attr_accessor :grid
 
   def initialize
-    @grid = []
+    @grid = Array.new(3) {Array.new(3)}
+    @valid_pieces = [:x, :o, nil]
+  end
+
+  def place(piece, row, column)
+    raise ('board is full') if self.full?
+    raise('invalid piece') if !@valid_pieces.include? piece
+    @grid[row][column] = piece 
+  end
+
+  def full?
+    !@grid.flatten.include? nil
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,10 +5,38 @@ describe Board do
   let (:board){Board.new}
 
   it 'has somewhere to store pieces' do
-    expect(board.grid[0]).to eq(nil)
+    expect(board.grid[0][0]).to eq(nil)
   end
 
+  it 'can store an object' do
+    board.place(:x, 0, 0)
+    expect(board.grid[0][0]).to eq(:x)
+  end
 
+  it 'can only store an x or an o, else it is nil' do
+    expect{board.place(:t, 0, 0)}.to raise_error('invalid piece')
+  end
 
+  it 'knows when it is full' do
+    fill_board
+    expect(board).to be_full
+  end
 
+  it 'cannot accept anymore pieces if full' do
+    fill_board
+    expect{board.place(:x, 0, 0)}.to raise_error('board is full')
+  end
+
+end
+
+def fill_board
+  board.place(:x, 0, 0)
+  board.place(:x, 0, 1)
+  board.place(:x, 0, 2)
+  board.place(:x, 1, 0)
+  board.place(:x, 1, 1)
+  board.place(:x, 1, 2)
+  board.place(:x, 2, 0)
+  board.place(:x, 2, 1)
+  board.place(:x, 2, 2)
 end
