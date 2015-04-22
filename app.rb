89e2f132af8a777536d2896ed542cc16fe92315f
Edward Brown <edward.brown.london@gmail.com>
2015-04-23 00:00:18 +0100

    some ajax stuffup and running

--- app.rb
+++ app.rb
@@ -1,15 +1,25 @@
 require 'sinatra'
 require_relative 'requirefile.rb'
 
-ed = Player.new
-bob = Player.new
+player_1 = Player.new
+player_1.choose(:player_1)
+player_2 = Player.new
+player_2.choose(:player_2)
 checker = Victory_checker.new
 board = Board.new
 game = Game.new
 
 get '/' do
-  board.place(:x, 0, 0)
-  board.place(:o, 1, 1)
+  player_1.place_piece(board, 2, 2)
   @board = board
   erb :main_page
+end
+
+post '/' do
+  board.reset!
+  redirect '/'
+end
+
+post '/place' do
+  player_1.place_piece(board, 2, 2)
 end
